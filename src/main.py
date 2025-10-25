9dd58bd0de34887b27666b3825055b25fdb71fe9
Yichen Zhou <zhouyichen30@gmail.com>
2025-10-25 11:44:56 -0400

    finish clean all the dateset needed for this project

--- src/main.py
+++ src/main.py
@@ -17,7 +17,10 @@ OUT_path = BASE_DIR / "outputs"
 cash_flow = pd.read_csv(data_path/ "cashflows_deal.csv")
 term_df = pd.read_csv(data_path/ "terms.csv")
 curve_df = pd.read_csv(data_path/ "curves.csv")
-#clean date columns
+structure_df = pd.read_csv(data_path/ "structure.csv")
+leverage_df = pd.read_csv(data_path/ "leverage.csv")
+
+#clean date, text, and number columns for all dataframe for this project
 cash_flow_date_cols = ['asof']
 cash_flow_text_cols = ['entity_id','entity_type','cashflow_type','currency']
 cash_flow_num_cols = ['amount']
@@ -29,16 +32,27 @@ term_data_num_cols = ['spread_bps','origination_fee_bps','OID_bps','amort_pct_pe
 curve_data_date_cols = ['asof']
 curve_data_text_cols = ['curve']
 curve_data_num_cols = ['rate']
+
+structure_data_date_cols=[]
+structure_data_text_cols=['facility_id','facility_name','deal_id','deal_name','strategy_bucket','region','sector','fund']
+structure_data_num_cols = []
+
+leverage__data_date_cols=[]
+leverage__data_text_cols=['fund','cost_of_funds_curve']
+leverage_data_num_cols = ['advance_rate','spread_bps','undrawn_fee_bps','commitment']
 #asof,curve,rate
 
 # ---- CLEANING ----
 cashflow_df = clean_data_format(cash_flow,cash_flow_date_cols,cash_flow_text_cols,cash_flow_num_cols)
-#futher clean cashflow data
+#further clean cashflow data
 cashflow_df_cleaned = cash_flow_sign_convert(cashflow_df)
 
 term_df = clean_data_format(term_df,term_data_date_cols,term_data_text_cols,term_data_num_cols)
+
 curve_df = clean_data_format(curve_df,curve_data_date_cols,curve_data_text_cols,curve_data_num_cols)
 #futher clean curve data
 curve_df_cleaned = clean_curve_df(curve_df)
 
-print(curve_df)
+structure_df = clean_data_format(structure_df,structure_data_date_cols,structure_data_text_cols,structure_data_num_cols)
+
+leverage_df = clean_data_format(leverage_df,leverage__data_date_cols,leverage__data_text_cols,leverage_data_num_cols)
